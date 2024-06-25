daf50f46ae479865dd27d30eacda569fd827f0a6
andresperezfc <felipe3apc@gmail.com>
2024-06-25 16:21:55 -0500

    Componentes en Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Laravel 11</title>
 </head>
+
 <body>
-    <h1>Página de Bienvenida</h1>
+    <div class="max-w-4xl mx-auto px-4 py-4">
+        <h1>Página de Bienvenida</h1>
+
+        <x-alert type="info">
+            <x-slot name="title">
+                Andrés
+            </x-slot>
+            Contenido de la alerta
+        </x-alert>
+    </div>
+
+
 </body>
+
 </html>
